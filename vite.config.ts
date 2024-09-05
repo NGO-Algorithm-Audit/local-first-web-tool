c5ee24e368bd93ccd022e5991efc5cb5a6d123c2
rbnlngs <langiusruben@gmail.com>
2024-09-05 12:32:30 +0200

    added packages for UI and there respective configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import path from 'path';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import viteReact from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ['pyodide']
-  },
-  plugins: [react()],
-  worker: {
-    format: 'es'
-  }
-})
+    optimizeDeps: {
+        exclude: ['pyodide'],
+    },
+    plugins: [TanStackRouterVite(), viteReact(), svgr()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    worker: {
+        format: 'es',
+    },
+});
