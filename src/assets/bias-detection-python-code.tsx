8dde68d3f3e029fdb2b9e2b33f78298625c87749
jfparie <jfparie@me.com>
2024-11-04 17:48:49 +0100

    minor edit Jurriaan

--- src/assets/bias-detection-python-code.tsx
+++ src/assets/bias-detection-python-code.tsx
@@ -159,6 +159,9 @@ def run():
 
     if isDemo:
         setResult(json.dumps(
+        {'type': 'heading', 'data': 'Information about demo dataset'}
+        ))
+        setResult(json.dumps(
             {'type': 'text', 'data': '''A demo dataset is loaded below. Using a subset of the [Twitter15](https://www.dropbox.com/s/7ewzdrbelpmrnxu/rumdetect2017.zip?e=1&file_subpath=%2Frumor_detection_acl2017%2FREADME.txt&st=mvtnttvx&dl=0) dataset, a BERT-based classifier has predicted, based on tweet characteristics, whether a message is fake news or not. False positive classifications are marked in the FP colomn and will be used as performance metric in this example.'''}
         ))
 
