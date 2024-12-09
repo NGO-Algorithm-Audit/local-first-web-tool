f2b759f856e50e3854c675e89739ca6ef689c9ac
Maikel van de Lisdonk <maikel@devhelpr.com>
2024-12-09 13:01:16 +0100

    tailwind config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     darkMode: ['class'],
     content: [
         './pages/**/*.{ts,tsx}',
@@ -41,5 +41,5 @@ module.exports = {
             },
         },
     },
-    plugins: [require('tailwindcss-animate')],
+    plugins: [import('tailwindcss-animate')],
 };
